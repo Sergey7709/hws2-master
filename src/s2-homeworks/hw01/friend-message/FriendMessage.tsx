9132b43be8c6d56674e6d560a68c2f672fd61d5a
Sergey Koshelev <shivacharya2019@gmail.com>
2022-12-11 17:23:49 +0300

    Initial commit

---
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import s from './FriendMessage.module.css'
+import {MessageType} from "../HW1";
+
+
+// создать тип вместо any и отобразить приходящие данные
+export type FriendMessagePropsType = {
+    message: MessageType
+}
+const FriendMessage = (props: FriendMessagePropsType) => {
+    return (
+        <div
+            id={'hw1-friend-message-' + props.message.id}
+            className={s.friendMessage}
+        >
+            <div className={s.friendImageAndText}>
+                <img
+                    id={'hw1-friend-avatar-' + props.message.id}
+                    src={props.message.user.avatar}
+                    alt ="avatar"
+                    // создаёт студент
+
+                    //
+                />
+                <div className={s.friendText}>
+                    <div
+                        id={'hw1-friend-name-' + props.message.id}
+                        className={s.friendName}
+                    >
+                        <span>{props.message.user.name}</span>
+                        {/*создаёт студент*/}
+
+                        {/**/}
+                    </div>
+                    <pre
+                        id={'hw1-friend-text-' + props.message.id}
+                        className={s.friendMessageText}
+                    >
+                        <p>{props.message.message.text}</p>
+                        {/*создаёт студент*/}
+
+                        {/**/}
+                    </pre>
+                </div>
+            </div>
+            <div
+                id={'hw1-friend-time-' + props.message.id}
+                className={s.friendTime}
+            >
+                <span>{props.message.message.time}</span>
+                {/*создаёт студент*/}
+
+                {/**/}
+            </div>
+        </div>
+    )
+}
+
+export default FriendMessage
