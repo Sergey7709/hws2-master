955a72f3bcac77ada4948bd35d49984ef83a6836
Sergey Koshelev <shivacharya2019@gmail.com>
2023-06-26 15:39:51 +0300

    HW-09

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,26 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: "sort"; payload: "up" | "down" }
+  | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType
+): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+      // by name
+      const sortedUsers = state.sort((a, b) => a.name.localeCompare(b.name));
+      return action.payload === "up" ? sortedUsers : sortedUsers.reverse();
+      // need to fix
     }
-}
+    case "check": {
+      return state.filter((user) => user.age > action.payload).reverse();
+      // need to fix
+    }
+    default:
+      return state;
+  }
+};
